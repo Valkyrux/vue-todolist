55744e437c42a3ced0937535175caaf079f2f71c
Valkyrux <philadephos.granata@gmail.com>
2021-12-17 12:27:04 +0100

    template

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        tasks: [
+            {
+                taskName: "Atask",
+                done: true
+            }
+        ]
+    }
+})
