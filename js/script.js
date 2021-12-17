622cd284155bac17102ce464a012404c3ffb3c05
Valkyrux <philadephos.granata@gmail.com>
2021-12-17 15:35:28 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -2,19 +2,15 @@ const app = new Vue({
     el: "#app",
     data: {
         inputText: "",
-        tasks: [
-            {
-                taskName: "Atask",
-                done: false
-            }
-        ]
+        tasks: []
     },
     methods: {
         addTask(){
             if(this.inputText != "") {
                 let aNewTask = {
                     taskName: this.inputText,
-                    done: false
+                    done: false,
+                    color: this.getRand()
                 }
                 this.tasks.unshift(aNewTask);
                 this.inputText ="";
@@ -25,6 +21,19 @@ const app = new Vue({
         },
         reverseDone(index){
             this.tasks[index].done = !this.tasks[index].done;
+        },
+        getRand(){
+            return Math.floor(Math.random()*5);
         }
+    },
+    created() {
+        this.tasks = [
+            {
+                taskName: "A task",
+                done: false,
+                color: this.getRand()
+            }
+        
+        ]
     }
 })
