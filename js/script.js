5503281c6b66fb378e0aaf8e29f21409633434a7
Valkyrux <philadephos.granata@gmail.com>
2021-12-17 12:59:00 +0100

    complete-withot-style

--- js/script.js
+++ js/script.js
@@ -1,11 +1,30 @@
 const app = new Vue({
     el: "#app",
     data: {
+        inputText: "",
         tasks: [
             {
                 taskName: "Atask",
-                done: true
+                done: false
             }
         ]
+    },
+    methods: {
+        addTask(){
+            if(this.inputText != "") {
+                let aNewTask = {
+                    taskName: this.inputText,
+                    done: false
+                }
+                this.tasks.unshift(aNewTask);
+                this.inputText ="";
+            }
+        },
+        removeTask(index){
+            this.tasks.splice(index, 1);
+        },
+        reverseDone(index){
+            this.tasks[index].done = !this.tasks[index].done;
+        }
     }
 })
